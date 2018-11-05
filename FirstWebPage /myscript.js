710e551c6537412ebb7a466ee3351eb50aeed3c0
manojkustommade <manoj.k@kustommade.com>
2018-11-05 11:44:14 +0530

    webpage

---
+++ FirstWebPage /myscript.js
@@ -0,0 +1,16 @@
+function myfun(){
+
+		document.getElementById("demo").innerHTML = "implement function in js use that name in attribute";
+}		
+function myfun2(){
+
+	var a,b,c;
+	var stringtype;
+	a=10;
+	b=20;
+	c=a+b;
+	stringtype='10'+'20';
+	document.getElementById("demo2").innerHTML=stringtype;
+	/*document.write("c");
+	document.write(stringtype);*/
+}
